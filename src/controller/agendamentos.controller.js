1d79bc9a56404898662f65f680551cd4f2a3d3c4
Gabriel/Korver <gabrielkorver76ers@gmail.com>
2025-11-28 11:08:33 -0300

    att funcao agendamento

--- src/controller/agendamentos.controller.js
+++ src/controller/agendamentos.controller.js
@@ -1,78 +1,89 @@
 import agendamentosRepository from "../repository/agendamentos.repository.js";
 
 const agendamentosController = {
-    async get() {
-        return agendamentosRepository.get();
-    },
+  async get() {
+    return agendamentosRepository.get();
+  },
 
-    async post(req, reply) {
-        try {
-            const { nome, telefone, data_agendamento, mentoria_id } = req.body;
+  async post(req, reply) {
+    try {
+      const { nome, telefone, data_agendamento, mentoria_id } = req.body;
 
-            const data_criacao = new Date().toISOString().slice(0, 19).replace("T", " ");
+      const data_criacao = new Date()
+        .toISOString()
+        .slice(0, 19)
+        .replace("T", " ");
 
-            // Campos obrigatórios
-            if (!nome || !telefone || !data_agendamento) {
-                return reply.status(400).send({
-                    message: "Campos obrigatórios faltando. (nome, telefone e data_agendamento)"
-                });
-            }
+      // Campos obrigatórios
+      if (!nome || !telefone || !data_agendamento) {
+        return reply.status(400).send({
+          message:
+            "Campos obrigatórios faltando. (nome, telefone e data_agendamento)",
+        });
+      }
 
-            // Validação telefone brasileiro
-            const telefoneRegex = /^\(?\d{2}\)?\s?\d{5}-?\d{4}$/;
+      // Validação telefone brasileiro
+      const telefoneRegex = /^\(?\d{2}\)?\s?\d{5}-?\d{4}$/;
 
-            if (!telefoneRegex.test(telefone)) {
-                return reply.status(400).send({
-                    message: "Telefone inválido. Use formato 11999999999 ou (11) 99999-9999"
-                });
-            }
+      if (!telefoneRegex.test(telefone)) {
+        return reply.status(400).send({
+          message:
+            "Telefone inválido. Use formato 11999999999 ou (11) 99999-9999",
+        });
+      }
 
-            // Data de agendamento
-            const agora = new Date();
-            const dataAgendamentoDate = new Date(data_agendamento);
+      // Data de agendamento
+      const agora = new Date();
+      const dataAgendamentoDate = new Date(data_agendamento);
 
-            if (isNaN(dataAgendamentoDate.getTime())) {
-                return reply.status(400).send({
-                    message: "Data de agendamento inválida"
-                });
-            }
+      if (isNaN(dataAgendamentoDate.getTime())) {
+        return reply.status(400).send({
+          message: "Data de agendamento inválida",
+        });
+      }
 
-            if (dataAgendamentoDate <= agora) {
-                return reply.status(400).send({
-                    message: "A data do agendamento deve ser futura"
-                });
-            }
+      if (dataAgendamentoDate <= agora) {
+        return reply.status(400).send({
+          message: "A data do agendamento deve ser futura",
+        });
+      }
 
-            // Criar agendamento
-            await agendamentosRepository.post(
-                nome,
-                telefone,
-                data_agendamento,
-                data_criacao,
-                mentoria_id
-            );
+      const existeAgendamento = await agendamentosRepository.findByDate(
+        data_agendamento
+      );
 
-            return reply.status(201).send({
-                message: "Agendamento cadastrado com sucesso!"
-            });
+      if (existeAgendamento) {
+        return reply.status(400).send({
+          message: "Já existe um agendamento nesse horário.",
+        });
+      }
 
-        } catch (error) {
-            console.error("❌ Erro no controller:", error);
+      // Criar agendamento
+      await agendamentosRepository.post(
+        nome,
+        telefone,
+        data_agendamento,
+        data_criacao,
+        mentoria_id
+      );
 
-            return reply.status(500).send({
-                message: "Erro interno no servidor. Tente novamente mais tarde."
-            });
-        }
-    },
+      return reply.status(201).send({
+        message: "Agendamento cadastrado com sucesso!",
+      });
+    } catch (error) {
+      console.error("❌ Erro no controller:", error);
 
-
-    async delete(req, reply) {
-        const { id } = req.params
-        await agendamentosRepository.delete(id)
-        return agendamentosRepository.delete()
+      return reply.status(500).send({
+        message: "Erro interno no servidor. Tente novamente mais tarde.",
+      });
     }
+  },
 
-
+  async delete(req, reply) {
+    const { id } = req.params;
+    await agendamentosRepository.delete(id);
+    return agendamentosRepository.delete();
+  },
 };
 
 export default agendamentosController;
--- src/repository/agendamentos.repository.js
+++ src/repository/agendamentos.repository.js
@@ -4,13 +4,13 @@
 import { neon } from "@neondatabase/serverless";
 
 const db = neon(
-    "postgresql://neondb_owner:npg_5ZqRKz6itcfU@ep-old-art-acs9eev0-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+  "postgresql://neondb_owner:npg_5ZqRKz6itcfU@ep-old-art-acs9eev0-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 );
 
 const agendamentosRepository = {
-    async get() {
-        try {
-            const agendamentos = await db`
+  async get() {
+    try {
+      const agendamentos = await db`
             SELECT
                 a.id,
                 a.nome,
@@ -25,25 +25,38 @@ const agendamentosRepository = {
                 mentoria AS m ON a.mentoria_id = m.id
         `;
 
-            return agendamentos;
+      return agendamentos;
+    } catch (error) {
+      console.error("❌ Erro ao buscar agendamentos:", error);
+      throw error;
+    }
+  },
 
-        } catch (error) {
-            console.error("❌ Erro ao buscar agendamentos:", error);
-            throw error;
-        }
-    },
-
-    async post(nome, telefone, data_agendamento, data_criacao, mentoria_id) {
-        return await db`INSERT INTO agendamentos(nome, telefone , data_agendamento, data_criacao, mentoria_id)
+  async post(nome, telefone, data_agendamento, data_criacao, mentoria_id) {
+    return await db`INSERT INTO agendamentos(nome, telefone , data_agendamento, data_criacao, mentoria_id)
         VALUES (${nome}, ${telefone}, ${data_agendamento}, ${data_criacao}, ${mentoria_id})
         `;
-    },
+  },
 
-    async delete(id) {
-        await db`DELETE FROM agendamentos WHERE id = ${id}`;
-        return { message: "Agendamento deletado com sucesso!" };
-    },
+  async findByDate(data_agendamento) {
+    try {
+      const result = await db`
+      SELECT *
+      FROM agendamentos
+      WHERE data_agendamento = ${data_agendamento}
+    `;
 
+      return result.length > 0 ? result[0] : null;
+    } catch (error) {
+      console.error("Erro ao verificar data:", error);
+      throw error;
+    }
+  },
+
+  async delete(id) {
+    await db`DELETE FROM agendamentos WHERE id = ${id}`;
+    return { message: "Agendamento deletado com sucesso!" };
+  },
 };
 
 export default agendamentosRepository;
